fc7e9e8cff5bcd65530c3aec1ca5e9688aea830a
shwetamaligithub <141333948+shwetamaligithub@users.noreply.github.com>
2025-03-11 22:11:39 +0530

    RoleReport.aspx.cs

---
+++ RoleReport.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Reporting.WebForms;
+
+public partial class RoleReport : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+
+
+        getdata();
+    }
+    public void getdata()
+    {
+        SqlConnection con = new SqlConnection(@"data source=.\SQLEXPRESS;Integrated Security=SSPI;AttachDBFilename=|DataDirectory|\aspnetdb.mdf;User Instance=true");
+        SqlCommand cmd = new SqlCommand("SELECT * FROM Role_Master", con);
+        cmd.Parameters.AddWithValue("@Rid", txtid.Text);
+        SqlDataAdapter da = new SqlDataAdapter(cmd);
+        DataTable dt = new DataTable("DataTable6");
+        da.Fill(dt);
+
+        ReportViewer1.ProcessingMode = ProcessingMode.Local;
+        ReportViewer1.LocalReport.ReportPath = Server.MapPath("RoleList.rdlc");
+        ReportViewer1.LocalReport.DataSources.Clear();
+        ReportViewer1.LocalReport.DataSources.Add(new ReportDataSource("DataSet1", dt));
+        ReportViewer1.LocalReport.Refresh();
+    }
+}

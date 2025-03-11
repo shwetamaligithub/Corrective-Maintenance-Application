8e2c94c46a4c4ce05f944d9c66fef468e13cfbb9
shwetamaligithub <141333948+shwetamaligithub@users.noreply.github.com>
2025-03-11 22:12:56 +0530

    TypeMaster.aspx.cs

---
+++ TypeMaster.aspx.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+public partial class TypeMaster : System.Web.UI.Page
+{
+    SqlConnection con = new SqlConnection(@"data source=.\SQLEXPRESS;Integrated Security=SSPI;AttachDBFilename=|DataDirectory|\aspnetdb.mdf;User Instance=true");
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!this.IsPostBack)
+        {
+            this.BindGrid();
+        }
+    }
+    private void BindGrid()
+    {
+        string constr = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+        string query = "SELECT * FROM Type_Master";
+        using (SqlConnection con = new SqlConnection(constr))
+        {
+            using (SqlCommand cmd = new SqlCommand(" SELECT * FROM Type_Master", con))
+            {
+                using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                {
+                    DataSet ds = new DataSet();
+                    da.Fill(ds, "Tid");
+                    if (ds.Tables["Tid"].Rows.Count > 0)
+                    {
+                        GridView1.DataSource = ds.Tables["Tid"];
+                        GridView1.DataBind();
+                    }
+                    con.Close();
+                }
+            }
+        }
+    }
+    protected void Button2_Click1(object sender, EventArgs e)
+    {
+        con.Open();
+        SqlCommand cmd = con.CreateCommand();
+        cmd.CommandType = CommandType.Text;
+        cmd.CommandText = "insert into Type_Master values('" + txtID.Text + "','" + txtName.Text + "','" + txtDescription.Text + "','" + txtDeptnm.Text + txtaction.SelectedItem.Value + "')";
+        cmd.ExecuteNonQuery();
+        con.Close();
+        Response.Write("<script>alert('Your Data has been successfully saved')</script>");
+    }
+    protected void Button5_Click(object sender, EventArgs e)
+    {
+        con.Open();
+        SqlCommand cmd = con.CreateCommand();
+        cmd.CommandType = CommandType.Text;
+        cmd.CommandText = ("UPDATE [dbo].[Type_Master] SET [Tid] ='" + txtID.Text + "',[Tnm]= '" + txtName.Text + "',[Des] ='" + txtDescription.Text + "',[Deptnm] ='" + txtDeptnm.Text + "',[Status] ='" + txtStatus.Text + "'WHERE [Tid]='" + txtID.Text + "'");
+        cmd.Connection = con;
+        cmd.ExecuteNonQuery();
+        con.Close();
+        Response.Write("<script>alert('Your Data has been successfully update')</script>");
+    }
+    protected void Button4_Click(object sender, EventArgs e)
+    {
+        con.Open();
+        SqlCommand cmd = con.CreateCommand();
+        cmd.CommandType = CommandType.Text;
+        if (txtStatus.Text.ToUpper() == "FALSE")
+        {
+            cmd.CommandText = "update Type_Master set Status = 'True'  where Tid=" + txtID.Text + " ";
+            Response.Write("<script>alert('Your Data has been successfully Activated')</script>");
+
+
+        }
+        else
+        {
+            cmd.CommandText = "update Type_Master set Status = 'False'  where Tid=" + txtID.Text + " ";
+            Response.Write("<script>alert('Your Data has been successfully Deactivated')</script>");
+        }
+        cmd.Connection = con;
+        cmd.ExecuteNonQuery();
+        this.BindGrid();
+    }
+    protected void Button3_Click1(object sender, EventArgs e)
+    {
+        txtID.Text = "";
+        txtName.Text = "";
+        txtDescription.Text = "";
+        txtDeptnm.Text = "";
+        txtaction.SelectedItem.Value = "";
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        if (txtaction.SelectedValue == "-1")
+        {
+            Response.Write("Please select an Type name");
+        }
+        else
+        {
+            string query = "select * from Type_Master where Status =@Status";
+            SqlDataAdapter sda = new SqlDataAdapter(query, con);
+            sda.SelectCommand.Parameters.AddWithValue("@Status", txtaction.SelectedItem.Value);
+            DataTable data = new DataTable();
+            sda.Fill(data);
+            GridView1.DataSource = data;
+            GridView1.DataBind();
+        }
+    }
+    protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        txtID.Text = GridView1.SelectedRow.Cells[1].Text;
+        txtName.Text = GridView1.SelectedRow.Cells[2].Text;
+        txtDescription.Text = GridView1.SelectedRow.Cells[3].Text;
+        txtDeptnm.Text = GridView1.SelectedRow.Cells[4].Text;
+        txtaction.SelectedItem.Value = GridView1.SelectedRow.Cells[5].Text;
+        txtStatus.Text = GridView1.SelectedRow.Cells[5].Text.ToUpper();
+        if (GridView1.SelectedRow.Cells[5].Text.ToUpper() == "FALSE")
+            Button4.Text = "Activate";
+        else
+            Button4.Text = "Deactivate";
+    }
+}

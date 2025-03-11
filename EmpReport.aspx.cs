71fb6f27feb7df723102adccb879729ae2752f04
shwetamaligithub <141333948+shwetamaligithub@users.noreply.github.com>
2025-03-11 21:24:37 +0530

    EmpReport.aspx.cs

---
+++ EmpReport.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Reporting.WebForms;
+
+public partial class EmpReport : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+
+
+        getdata();
+    }
+    public void getdata()
+    {
+        SqlConnection con = new SqlConnection(@"data source=.\SQLEXPRESS;Integrated Security=SSPI;AttachDBFilename=|DataDirectory|\aspnetdb.mdf;User Instance=true");
+        SqlCommand cmd = new SqlCommand("SELECT * FROM Employee_Master where Eid=@Eid", con);
+        cmd.Parameters.AddWithValue("@Eid", txtid.Text);
+        SqlDataAdapter da = new SqlDataAdapter(cmd);
+        DataTable dt = new DataTable("DataTable1");
+        da.Fill(dt);
+
+        ReportViewer1.ProcessingMode = ProcessingMode.Local;
+        ReportViewer1.LocalReport.ReportPath = Server.MapPath("EmpList.rdlc");
+        ReportViewer1.LocalReport.DataSources.Clear();
+        ReportViewer1.LocalReport.DataSources.Add(new ReportDataSource("DataSet1", dt));
+        ReportViewer1.LocalReport.Refresh();
+    }
+
+
+   
+}
